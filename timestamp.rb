5a7d41483f4b2fcb9b21049879aa6bb7342797e4
Luis Henrique Mulinari <luis.mulinari@gmail.com>
2010-07-07 20:54:34 -0300

    Primeira versao que compila basico completo, mas nao roda

--- timestamp.rb
+++ timestamp.rb
@@ -1,26 +1,51 @@
 class Transacao
 
-  attr_reader :id, :ts
+  attr_reader :id, :ts, :historia
 
   def initialize(historia, ts, id)
 
-    @actions = historia.split ' '
+    @historia = historia
+    @operations = []
+    @historia.split(' ').each { |a| @operations.push Operacao.new(a[0], a[2]) }
     @ts = ts
     @id = id
   end
 
   def next_operation
-  @actions.first
+    @operations.first
   end
 
   def finished?
+    @operations.empty?
   end
 
-  def execute
+  def execute(dado)
+     op = @operations.first
+     @operations.delete_at(0)
+
+     if op.type == 'r' then
+       if dado.ts_read < @ts then
+         dado.ts_read = @ts
+       end
+     else
+         dado.ts_write = @ts
+     end
+     p 'Executada thread: ', @id.to_s, '; timestamp: ', @ts, '; dado: ', dado.value
   end
 
   def abort
+    p 'abortando thread: ', @id.to_s, '; timestamp: ', @ts
   end
+end
+
+class Operacao
+
+ attr_reader :type, :dado
+
+ def initialize type, dado
+   @type = type
+   @dado = dado
+ end
 end
 
 class Dado
@@ -44,11 +69,42 @@ class Scheduler
 
     while transactions.empty? do
 
-      t = transactions[rand(transactions.size)]
+      posicao = rand(transactions.size)
+      t = transactions[posicao]
       p "Executando thread: ", t.id.to_s
       operation = t.next_operation
 
-    end
+      if operation.type == 'r' then
+
+        if (!dados.has_key(operation.dado)) then
+          dado[operation.dado] = Dado.new(operation.dado, Time.new.to_i, 0, t.id)
+        end
+
+        d = dado[operation.dado]
+        if t.ts < d.ts_write then
+          t.abort
+          transactions[posicao] = Transaction.new(t.historia, Time.new.to_i, t.id)
+        else
+          t.execute d
+          if t.finished? then
+            transactions.delete_at posicao
+            p "Commiting thread: ", t.id.to_s
+          end # finished
+        end # ts comparison
+
+      else
+        if ((t.ts < d.ts_read) || (t.ts < d.ts_write)) then
+          t.abort
+          transactions[posicao] = Transaction.new(t.historia, Time.new.to_i, t.id)
+        else
+          t.execute d
+          if t.finished? then
+            transactions.delete_at posicao
+            p "Commiting thread: ", t.id.to_s
+          end
+        end #ts comparison
+      end # operation type
+    end #while
   end
 end
 
