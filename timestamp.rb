9c86e840df33cc9c7fc7a9454f932d33e604acd2
Luis Henrique Mulinari <luis.mulinari@gmail.com>
2010-07-07 20:07:17 -0300

    Adicionando um arquivo exemplo de transacoes e comecando o scheduler

--- timestamp.rb
+++ timestamp.rb
@@ -1,28 +1,61 @@
 class Transacao
 
- def init(historia, ts, id)
- end
+  attr_reader :id, :ts
 
- def next_action
- end
+  def initialize(historia, ts, id)
 
- def finished?
- end
+    @actions = historia.split ' '
+    @ts = ts
+    @id = id
+  end
 
- def execute
- end
+  def next_operation
+  @actions.first
+  end
 
- def abort
- end
+  def finished?
+  end
+
+  def execute
+  end
+
+  def abort
+  end
+end
+
+class Dado
+
+  attr_reader :value
+  attr_accessor :ts_read, :ts_write, :current_transaction
+
+  def initialize value, ts_read, ts_write, current_transaction
+    @value = value
+    @ts_read = ts_read
+    @ts_write = ts_write
+    @current_transaction = current_transaction
+  end
 end
 
 class Scheduler
+
+  def self.schedule transactions
+
+    dados = []
+
+    while transactions.empty? do
+
+      t = transactions[rand(transactions.size)]
+      p "Executando thread: ", t.id.to_s
+      operation = t.next_operation
+
+    end
+  end
 end
 
 transactions = []
 f = File.new(ARGV[0])
 f.each_line { |line|
-
-    transactions[] = new Transacao(line, Time.new.to_i, f.lineno)
-   
+  transactions.push Transacao.new(line, Time.new.to_i, f.lineno)
 }
+
+Scheduler.schedule(transactions)
